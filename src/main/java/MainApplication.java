a80a22e09b9d539cc6dcb4d7806e7b727839acc0
sankalp-jain <sankalp.jain2016@vitstudent.ac.in>
2020-06-17 23:05:49 +0530

    Converted java to maven project

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,19 @@
+import validations.FileValidation;
+
+import java.util.Scanner;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input file path");
+        String fileName = sc.nextLine();
+
+        boolean checkFileValidity = FileValidation.isValidFile(fileName);
+        if(checkFileValidity) {
+            System.out.println("Valid File");
+        }
+        else {
+            System.out.println("Not a Valid file");
+        }
+    }
+}
---
+++ src/main/java/validations/FileValidation.java
@@ -0,0 +1,48 @@
+package validations;
+import java.io.File;
+
+public class FileValidation {
+    public static boolean isFileExists(File file) {
+        if(file.exists()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean isValidSize(File file) {
+        long fileSize = file.length();
+        if(fileSize > 0 && fileSize < 5 * 1024 * 1024) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean isValidExtension(File file) {
+        String[] validExtensions = {"pdf", "doc", "txt"};
+        String name = file.getName();
+        String fileExtension = "";
+        if(name.lastIndexOf(".") != -1 && name.lastIndexOf(".") != 0)
+            fileExtension = name.substring(name.lastIndexOf(".") + 1);
+
+        for (int i = 0; i < validExtensions.length; i++) {
+            if(fileExtension.equals(validExtensions[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isValidFile(String filename) {
+        File file = new File(filename);
+        if(isFileExists(file) && isValidSize(file) && isValidExtension(file)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
