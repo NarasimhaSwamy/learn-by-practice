b840dbb2c147d657ea1fc25ddc798e7a8729888e
sankalp-jain <sankalp.jain2016@vitstudent.ac.in>
2020-06-27 10:03:27 +0530

    Updated FileValidation.java

--- src/main/java/utils/FileValidation.java
+++ src/main/java/utils/FileValidation.java
@@ -2,46 +2,29 @@ package utils;
 import java.io.File;
 
 public class FileValidation {
-    public static boolean isFileExists(File file) {
-        if(file.exists()) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+    private static final long MAX_FILE_SIZE = 5 * 1024 * 1024;
+    private static final String[] VALID_FILE_EXTENSIONS = {"pdf", "doc", "txt"};
 
-    public static boolean isValidSize(File file) {
-        long fileSize = file.length();
-        if(fileSize > 0 && fileSize < 5 * 1024 * 1024) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public static boolean isValidExtension(File file) {
-        String[] validExtensions = {"pdf", "doc", "txt"};
+    private static final Predicate<File> FILE_EXISTENCE_PREDICATE = File::exists;
+    private static final Predicate<File> MAX_FILE_SIZE_PREDICATE = file -> {
+        long fileLength = file.length();
+        return (fileLength>0 && fileLength< MAX_FILE_SIZE);
+    };
+    private static final Predicate<File> VALID_FILE_EXTENSION_PREDICATE = file -> {
         String name = file.getName();
         String fileExtension = "";
         if(name.lastIndexOf(".") != -1 && name.lastIndexOf(".") != 0)
             fileExtension = name.substring(name.lastIndexOf(".") + 1);
 
-        for (int i = 0; i < validExtensions.length; i++) {
-            if(fileExtension.equals(validExtensions[i])) {
+        for (int i = 0; i < VALID_FILE_EXTENSIONS.length; i++) {
+            if(fileExtension.equals(VALID_FILE_EXTENSIONS[i])) {
                 return true;
             }
         }
         return false;
-    }
+    };
 
     public static boolean isValidFile(File file) {
-        if(isFileExists(file) && isValidSize(file) && isValidExtension(file)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return FILE_EXISTENCE_PREDICATE.and(MAX_FILE_SIZE_PREDICATE).and(VALID_FILE_EXTENSION_PREDICATE).test(file);
     }
 }
